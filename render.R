f8a36f412ccbaf7470fc46e5ae98eff4a77afe61
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-04-16 23:58:24 -0400

    duckdb

--- render.R
+++ render.R
@@ -1,4 +1,4 @@
-pkgs <- c("mlr3verse","fastverse","ranger","tidyfast","dtplyr","data.table", "magrittr", "palmerpenguins", "tidyverse", "knitr", "dplyr", "reshape2", "atime", "ggplot2", "reticulate", "quarto", "kknn", "nc")
+pkgs <- c("mlr3verse","fastverse","ranger","tidyfast","dtplyr","data.table", "magrittr", "palmerpenguins", "tidyverse", "knitr", "dplyr", "reshape2", "atime", "ggplot2", "reticulate", "quarto", "kknn", "nc", "duckdb")
 ins.mat <- installed.packages()
 missing.pkgs <- setdiff(pkgs, rownames(ins.mat))
 install.packages(missing.pkgs)
